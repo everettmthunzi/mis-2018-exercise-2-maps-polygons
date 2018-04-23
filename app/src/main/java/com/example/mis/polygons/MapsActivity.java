1aee569eed96d76135cb52d80e9cb21e884979e9
Lars Meyer <lars.meyer@uni-weimar.de>
2018-04-23 11:50:17 +0200

    Add polygon calculations and interaction stubs

--- app/src/main/java/com/example/mis/polygons/MapsActivity.java
+++ app/src/main/java/com/example/mis/polygons/MapsActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,8 +24,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
 
@@ -58,6 +66,178 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
+    // PolygonOptions, will hold all vertices of the polygon
+    private PolygonOptions polyOpts = new PolygonOptions();
+    // var referencing button that initiates polygon capture/drawing
+    private Button polygonButton;
+    // Boolean signifying ability to add vertices (markers) to Polygon
+    private Boolean polygonCaptureEnabled = false;
+
+    /* OnClickListener for starting a Polygon
+     * (general syntax via https://stackoverflow.com/a/26147707)
+     */
+    private View.OnClickListener startPolygon = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            // Notify the user that a Polygon has started
+            Toast polygonStartToast = Toast.makeText(MapsActivity.this,
+                    "Polygon capture started", Toast.LENGTH_SHORT);
+            polygonStartToast.show();
+
+            // Change the text of the button
+            polygonButton.setText("End Polygon");
+
+            // Set its onClick listener to endPolygon
+            polygonButton.setOnClickListener(endPolygon);
+
+            // Clear out the existing PolygonOptions
+            polyOpts = new PolygonOptions();
+
+            // make it semitransparent blue
+            polyOpts.fillColor(0x800000FF);
+
+            // enable Polygon capture
+            polygonCaptureEnabled = true;
+        }
+
+    };
+
+    // OnClickListener for ending a Polygon
+    private View.OnClickListener endPolygon = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view){
+            // Notify the user that a polygon has been constructed
+            Toast polygonEndToast = Toast.makeText(MapsActivity.this,
+                    "Drawing Polygon...", Toast.LENGTH_SHORT);
+            polygonEndToast.show();
+
+            // Change the text of the button
+            polygonButton.setText("Start Polygon");
+
+            // Set its onClick listener to startPolygon
+            polygonButton.setOnClickListener(startPolygon);
+
+            // disable Polygon capture
+            polygonCaptureEnabled = false;
+
+            if (polyOpts.getPoints().size() > 1) {
+                // show Polygon
+                Polygon polygon = mMap.addPolygon(polyOpts);
+
+                // calculate surface area of Polygon
+                Thread surfaceCalculation = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Double area = calculatePolygonArea(polyOpts.getPoints());
+
+                        // make units sane
+                        String title;
+                        if (area > 1000) {
+                            title = (area / 1000) + "km²";
+                        } else {
+                            title = area + "m²";
+                        }
+                        final String displayArea = title;
+
+                        final LatLng centroid = polygonCentroid(polyOpts.getPoints());
+
+                        // display area as marker at Polygon centroid
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(centroid)
+                                        .title(displayArea));
+                            }
+                        });
+                    }
+                });
+            }
+            else {
+                Toast tooFewMarkersCaptured = Toast.makeText(MapsActivity.this,
+                        "Too few markers captured!", Toast.LENGTH_SHORT);
+                tooFewMarkersCaptured.show();
+            }
+        }
+
+    };
+
+    // Method to calculate the area of a Polygon, base unit m²
+    private Double calculatePolygonArea(List<LatLng> vertices) {
+        // List of vertices relative to the first one, distance in meters
+        List<Pair<Double, Double>> vm = new LinkedList<>();
+        // add first vertex
+        vm.add(new Pair<>(0.0, 0.0));
+
+        // first vertex, all others relative to this one
+        LatLng v0 = vertices.get(0);
+        // see below
+        Double xFactor1 = Math.cos(Math.toRadians(v0.latitude)) * 111321;
+
+        // for loop traversing through vertices and making them relative
+        for (int i = 1; i < vertices.size(); i++) {
+            // get vertex
+            LatLng v1 = vertices.get(i);
+
+            /* Find out how long (in m) a degree of longitude is and use it as
+             * a factor to find out the x distance between two LatLongs
+             * (via https://gis.stackexchange.com/a/142327)
+             */
+            Double xFactor2 = Math.cos(Math.toRadians(v1.latitude)) * 111321;
+            // average the two factors to account for differences in latitude
+            Double xFactor = (xFactor1 + xFactor2) / 2.0;
+
+            // Calculate x and y distances (coordinates) relative to v0
+            Double x = (v1.longitude - v0.longitude) * xFactor;
+            Double y = (v1.latitude - v0.latitude) * 111000;
+
+            // add new relative vertex to list
+            vm.add(new Pair<>(x, y));
+        }
+
+        // accumulated area, to be divided by 2 later
+        Double area = 0.0;
+
+        /* for loop traversing through relative vertices and calculating the
+         * area of the polygon in m²
+         * (via https://www.mathopenref.com/coordpolygonarea.html)
+         */
+        for (int i = 0; i < vm.size()-1; i = i+2) {
+            // get pair of vertices
+            Pair<Double, Double> vm1 = vm.get(i);
+            Pair<Double, Double> vm2;
+            if (i+1 < vm.size()) {
+                vm2 = vm.get(i+1);
+            }
+            else {
+                vm2 = vm.get(0);
+            }
+
+            // calculate area fragment (not yet divided by 2)
+            Double frag = (vm1.first * vm2.second) - (vm1.second * vm2.first);
+            area += frag;
+        }
+
+        return Math.abs(area/2.0);
+    }
+
+    /* Find centroid of polygon
+     * (via https://math.stackexchange.com/a/699413)
+     */
+    private LatLng polygonCentroid(List<LatLng> vertices) {
+        Double latitudes = 0.0;
+        Double longitudes = 0.0;
+
+        for (LatLng v : vertices) {
+            latitudes += v.latitude;
+            longitudes += v.longitude;
+        }
+
+        return new LatLng(latitudes/vertices.size(),
+                longitudes/vertices.size());
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -65,6 +245,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_maps);
 
         getLocationPermission();
+
+        // Reference the polygon button from activity_main.xml
+        polygonButton = findViewById(R.id.polygonButton);
+        /* somehow unable to set onClick listener in Android Studio UI,
+         * doing it the hard way
+         */
+        polygonButton.setOnClickListener(startPolygon);
     }
 
     private void getDeviceLocation(){
